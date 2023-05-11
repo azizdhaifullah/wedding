80e2488e086cb605310ef5454680b56ac8a48bef
aziz_dhaifullah <aziz@investree.id>
2023-05-11 23:12:01 +0700

    Basic Index

---
+++ src/components/Headers/index.tsx
@@ -0,0 +1,13 @@
+export default function Headers() {
+  return (
+    <header>
+      <ul>
+        <li>Home</li>
+        <li>Gallery</li>
+        <li>Location</li>
+        <li>Donation</li>
+        <li>Greetings</li>
+      </ul>
+    </header>
+  )
+}
