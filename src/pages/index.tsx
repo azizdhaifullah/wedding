cf0702d348e1d535338862f8e0e9725266399a41
aziz_dhaifullah <aziz@investree.id>
2023-06-17 10:06:21 +0700

    Update API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
-import Headers from '@/components/Headers'
 
 export default function Home() {
   return (
     <>
-      <Headers />
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
